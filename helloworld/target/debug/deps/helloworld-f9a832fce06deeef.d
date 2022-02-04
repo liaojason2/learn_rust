9ccdf2b25fff62bcc442278c974945e92bc3f111
Huan-Chieh Liao <liao.jason2@gmail.com>
2022-02-04 16:43:44 +0800

    Add Helloworld

---
+++ helloworld/target/debug/deps/helloworld-f9a832fce06deeef.d
@@ -0,0 +1,5 @@
+/Users/jason/Desktop/Rust/helloworld/target/debug/deps/helloworld-f9a832fce06deeef: src/main.rs
+
+/Users/jason/Desktop/Rust/helloworld/target/debug/deps/helloworld-f9a832fce06deeef.d: src/main.rs
+
+src/main.rs:
---
+++ helloworld/target/debug/helloworld.d
@@ -0,0 +1,1 @@
+/Users/jason/Desktop/Rust/helloworld/target/debug/helloworld: /Users/jason/Desktop/Rust/helloworld/src/main.rs
